006820c60e02b96c45f2118d5074b412e711026b
KaioCosta62 <kaiohenriqueranking1@gmail.com>
2022-11-16 18:53:17 -0300

    corrigindo erros do banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function connect(){
-  mongoose.connect('mongodb://localhost:27017/lanchonete-kaiao')
+  mongoose.connect('mongodb://127.0.0.1:27017/lanchonete-kaiao')
   const db = mongoose.connection
 
   db.once('open', () => {
